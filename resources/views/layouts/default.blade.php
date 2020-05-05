08bdcbba519c3fba9a9adab250b9d038b5435bca
vagrant <vagrant@homestead>
2020-05-05 10:51:12 +0000

    静态文件浏览器缓存 问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App')  - Laravel 新手入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
