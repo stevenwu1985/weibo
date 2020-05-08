490c6c5b4de06eadda1ebdbc2ad87847c4e55cf6
vagrant <vagrant@homestead>
2020-05-08 06:05:56 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StaticPagesController extends Controller
 {
-
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(10);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
@@ -21,5 +27,4 @@ class StaticPagesController extends Controller
     {
         return view('static_pages/about');
     }
-
 }
