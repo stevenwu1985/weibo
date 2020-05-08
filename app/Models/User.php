c362eafe4a1fa9dcb527ba8860c98585ed1d4e8b
vagrant <vagrant@homestead>
2020-05-08 02:11:31 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
